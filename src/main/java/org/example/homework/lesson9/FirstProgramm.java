92b14957a92c08704eeb09515eb95c6203996cf0
my.h.m@yandex.ru <Zxcv7788jetbrains>
2023-06-11 20:47:59 +0300

    Lection9

---
+++ src/main/java/org/example/homework/lesson9/FirstProgramm.java
@@ -0,0 +1,22 @@
+package org.example.homework.lesson9;
+
+public class FirstProgramm {
+
+    public static void abc() {
+        String s1 = new String("ABC");
+        String s2 = new String("DEF");
+
+    }
+
+    public static void main(String[] args) {
+        FirstProgramm fP = new FirstProgramm();
+        abc();
+        abc();
+        String s1 = new String("ABC");
+        abc();
+
+    }
+
+
+}
+

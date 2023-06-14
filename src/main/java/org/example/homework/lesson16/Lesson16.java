0eba09694f990116cc14b1468f5878828754d881
my.h.m@yandex.ru <Zxcv7788jetbrains>
2023-06-14 14:29:44 +0300

    Lection16

---
+++ src/main/java/org/example/homework/lesson16/Lesson16.java
@@ -0,0 +1,27 @@
+package org.example.homework.lesson16;
+
+public class Lesson16 {
+
+
+    public void email(String s) {
+        int a = 0;
+        int b = 0;
+        int c = 0;
+
+        while (c < s.length() - 1) {
+
+            a = s.indexOf('@', c);
+            b = s.indexOf('.', c);
+            c = s.indexOf(';', c + 1);
+            System.out.println(s.substring(a + 1, b ));
+        }
+    }
+
+    public static void main(String[] args) {
+        Lesson16 ls = new Lesson16();
+        ls.email("ya@yahoo.com; on@mail.ru; ona@gmail.com;");
+    }
+}
+
+
+

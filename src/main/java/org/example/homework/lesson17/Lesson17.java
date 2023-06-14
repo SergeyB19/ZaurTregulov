18ad7af76abccf925b8e968609d6153d9353ce1a
my.h.m@yandex.ru <Zxcv7788jetbrains>
2023-06-14 18:44:48 +0300

    Lection17

---
+++ src/main/java/org/example/homework/lesson17/Lesson17.java
@@ -0,0 +1,30 @@
+package org.example.homework.lesson17;
+
+public class Lesson17 {
+
+
+
+    static boolean ravenstvo(StringBuilder sb1, StringBuilder sb2) {
+        boolean result = true;
+        if (sb1.length() == sb2.length()) {
+            for (int i = 0; i < sb1.length(); i++) {
+                if (sb1.charAt(i) != sb2.charAt(i)) {
+                    result = false;
+                    break;
+                }
+            }
+        } else {
+            result = false;
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        StringBuilder sb1 = new StringBuilder("Hello");
+        StringBuilder sb2 = new StringBuilder("Hello");
+       boolean a = Lesson17.ravenstvo(sb1, sb2);
+        System.out.println(a);
+    }
+
+}

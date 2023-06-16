735bcbec9f84f735256e70929dc089930f9f8815
my.h.m@yandex.ru <Zxcv7788jetbrains>
2023-06-16 11:28:13 +0300

    Lection19

---
+++ src/main/java/org/example/homework/lesson19/Lesson19.java
@@ -0,0 +1,37 @@
+package org.example.homework.lesson19;
+
+import java.util.Arrays;
+
+public class Lesson19 {
+
+    public static String[] abc(String[]... array1) {
+        int length = 0;
+        for (String[] array2 : array1) {
+            length += array2.length;
+        }
+        String[] target = new String[length];
+        int count = 0;
+        for (String[] array2 : array1) {
+            for (String s : array2) {
+                target[count] = s;
+                count++;
+            }
+        }
+        return target;
+    }
+
+    public static void main(String[] args) {
+        String[] target = abc(new String[]{"ok", "привет", "пока"}, new String[]{"ok", "hello", "bye"});
+        for (String s: args) {
+            for (int i = 0; i < target.length ; i++) {
+                if (s.equals(target[i])) {
+                    target[i] = null;
+                }
+            }
+        }
+        for (String s: target) {
+            System.out.println(s+" ");
+        }
+        System.out.println();
+    }
+}

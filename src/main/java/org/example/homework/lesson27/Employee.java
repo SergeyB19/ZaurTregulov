8c82cf79b45af265c8a72726240b80d40262342d
my.h.m@yandex.ru <Zxcv7788jetbrains>
2023-06-21 18:37:37 +0300

    Lection27

---
+++ src/main/java/org/example/homework/lesson27/Employee.java
@@ -0,0 +1,51 @@
+package org.example.homework.lesson27;
+
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+public class Employee {
+    String name;
+
+    String department;
+    double salary;
+
+    public Employee(String name, String department, double salary) {
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+}
+
+class TestEmployee {
+
+    public static void main(String[] args) {
+
+        Employee employee1 = new Employee("Ameli", "IT", 1_700_000);
+        Employee employee2 = new Employee("Rozamund", "3", 1_550_000);
+        Employee employee3 = new Employee("Lili", "5", 1_930_000);
+        ArrayList<Employee> list = new ArrayList<>();
+        list.add(employee1);
+        list.add(employee2);
+        list.add(employee3);
+
+        TestEmployee testEmployee = new TestEmployee();
+        testEmployee.filtraciyaRabotnikov(arrayList, (Employee em)->{return em.department=="IT" && em.salary>200;});
+        System.out.println("__________________________________");
+        testEmployee.filtraciyaRabotnikov(arrayList, (Employee em)->{return  em.name.startsWith("E")&& em.salary!=450;} );
+        System.out.println("__________________________________");
+        testEmployee.filtraciyaRabotnikov(arrayList, (Employee em)->{return  em.name.equals(em.department);});
+
+
+    public static void printEmployee(Employee emp) {
+        System.out.print(emp.name+ " " + emp.department + " "+emp.salary);
+    }
+
+        public static void filtraciyaRabotnikov (ArrayList < Employee > arrayList, Predicate < Employee > predicate){
+            for (Employee emp : arrayList) {
+                if (predicate.test(emp)) {
+                    printEmployee(emp);
+                }
+            }
+        }
+    }
+}

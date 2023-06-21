49de948c5407263100977c5533041858b36c1629
my.h.m@yandex.ru <Zxcv7788jetbrains>
2023-06-21 19:27:27 +0300

    Lection27

--- src/main/java/org/example/homework/lesson27/Employee.java
+++ src/main/java/org/example/homework/lesson27/Employee.java
@@ -23,28 +23,27 @@ class TestEmployee {
         Employee employee1 = new Employee("Ameli", "IT", 1_700_000);
         Employee employee2 = new Employee("Rozamund", "3", 1_550_000);
         Employee employee3 = new Employee("Lili", "5", 1_930_000);
-        ArrayList<Employee> list = new ArrayList<>();
-        list.add(employee1);
-        list.add(employee2);
-        list.add(employee3);
+        ArrayList<Employee> arrayList = new ArrayList<>();
+        arrayList.add(employee1);
+        arrayList.add(employee2);
+        arrayList.add(employee3);
 
         TestEmployee testEmployee = new TestEmployee();
-        testEmployee.filtraciyaRabotnikov(arrayList, (Employee em)->{return em.department=="IT" && em.salary>200;});
+        testEmployee.filtraciyaRabotnikov(arrayList, (Employee em) -> em.department.equals("IT") && em.salary > 200);
         System.out.println("__________________________________");
-        testEmployee.filtraciyaRabotnikov(arrayList, (Employee em)->{return  em.name.startsWith("E")&& em.salary!=450;} );
+        testEmployee.filtraciyaRabotnikov(arrayList, (Employee em) -> em.name.startsWith("E") && em.salary != 450);
         System.out.println("__________________________________");
-        testEmployee.filtraciyaRabotnikov(arrayList, (Employee em)->{return  em.name.equals(em.department);});
+        testEmployee.filtraciyaRabotnikov(arrayList, (Employee em) -> em.name.equals(em.department));
 
-
-    public static void printEmployee(Employee emp) {
-        System.out.print(emp.name+ " " + emp.department + " "+emp.salary);
+    }
+    public static void printEmployee (Employee emp){
+        System.out.print(emp.name + " " + emp.department + " " + emp.salary);
     }
 
-        public static void filtraciyaRabotnikov (ArrayList < Employee > arrayList, Predicate < Employee > predicate){
-            for (Employee emp : arrayList) {
-                if (predicate.test(emp)) {
-                    printEmployee(emp);
-                }
+    public static void filtraciyaRabotnikov (ArrayList < Employee > arrayList, Predicate < Employee > predicate){
+        for (Employee emp : arrayList) {
+            if (predicate.test(emp)) {
+                printEmployee(emp);
             }
         }
     }
